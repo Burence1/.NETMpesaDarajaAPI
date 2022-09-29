ef85ac82085c8de7e9a7ef3e91804558464b89bb
Burence1 <burensomondi@gmail.com>
2022-09-29 16:45:42 +0300

    Add account balance enum

---
+++ Enums/IdentifierType.cs
@@ -0,0 +1,12 @@
+﻿namespace MpesaDarajaAPI.Enums;
+
+public enum IdentifierType
+{
+    /// <summary>
+    /// IdentifierTypes For Account Balance endpoint
+    /// </summary>
+
+    MSISDN = 1,
+    TillNumber = 2,
+    Shortcode = 4,
+}
