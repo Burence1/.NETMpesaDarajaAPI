00af836010f29143b95ed036607dfad779a38664
Burence1 <burensomondi@gmail.com>
2022-10-18 15:39:35 +0300

    Add reposnes

---
+++ Dtos/Reponses/STKPushQueryResponse.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace MpesaDarajaAPI.Dtos.Reponses
+{
+    public class STKPushQueryResponse
+    {
+        [JsonProperty("IsSuccess")]
+        public bool IsSuccess { get; set; }
+        [JsonProperty("SuccessResponse")]
+        public StkPushQuerySuccessResponse SuccessResponse { get; set; }
+        [JsonProperty("ErrorResponse")]
+        public ErrorResponse ErrorResponse { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
