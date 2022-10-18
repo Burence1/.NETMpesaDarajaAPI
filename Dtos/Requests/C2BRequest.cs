4c4f45aade97bba4669f61bb3376ec75d0d38293
Burence1 <burensomondi@gmail.com>
2022-10-18 15:25:43 +0300

    Add B2B, B2C and C2B requests

---
+++ Dtos/Requests/C2BRequest.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace MpesaDarajaAPI.Dtos.Requests
+{
+    public class C2BRequest
+    {
+        [JsonProperty("CommandID")]
+        public string CommandID { get; set; }
+        [JsonProperty("Amount")]
+        public long Amount { get; set; }
+        
+        [JsonProperty("Msisdn")]
+        public string MSISDN { get; set; }
+        [JsonProperty("BillRefNumber")]
+        public string BillRefNumber { get; set; }
+        [JsonProperty("ShortCode")]
+        public string ShortCode { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
