7ecf38e2fc05b111f04aacc0f5d1e83e30447ee3
Burence1 <burensomondi@gmail.com>
2022-10-18 15:11:58 +0300

    update requests

---
+++ Dtos/Requests/C2BRegisterUrlRequest.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace MpesaDarajaAPI.Dtos.Requests
+{
+    public class C2BRegisterUrlRequest
+    {
+        [JsonProperty("ValidationURL")]
+        public string validationURL { get; set; }
+
+        [JsonProperty("ConfirmationURL")]
+        public string confirmationURL { get; set; }
+
+        [JsonProperty("ResponseType")]
+        public string responseType { get; set; }
+
+        [JsonProperty("ShortCode")]
+        public string shortCode { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
