9b342851a431a4061caa6fc2c8116896bad04a1f
Burence1 <burensomondi@gmail.com>
2022-10-18 15:37:43 +0300

    Add responses

---
+++ Dtos/Reponses/STKPushResponse.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace MpesaDarajaAPI.Dtos.Reponses
+{
+    public class STKPushResponse
+    {
+        [JsonProperty("IsSuccess")]
+        public bool IsSuccess { get; set; }
+        [JsonProperty("SuccessResponse")]
+        public StkPushSuccessResponse SuccessResponse { get; set; }
+        [JsonProperty("ErrorResponse")]
+        public ErrorResponse ErrorResponse { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
--- Dtos/Reponses/SuccessResponse.cs
+++ Dtos/Reponses/SuccessResponse.cs
@@ -2,29 +2,64 @@
 
 namespace MpesaDarajaAPI.Dtos.Reponses
 {
-    public class SuccessResponse
+    
+    public class BaseSuccessResponse
     {
-        public class BaseSuccessResponse
-        {
-            [JsonProperty("ResponseDescription")]
-            public string ResponseDescription { get; set; }
-            [JsonProperty("ResponseCode")]
-            public string ResponseCode { get; set; }
+        [JsonProperty("ResponseDescription")]
+        public string ResponseDescription { get; set; }
+        [JsonProperty("ResponseCode")]
+        public string ResponseCode { get; set; }
 
-            public override string ToString()
-            {
-                return JsonConvert.SerializeObject(this);
-            }
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
         }
     }
+    
 
-    public class CommonSuccessResponse : SuccessResponse.BaseSuccessResponse
+    public class CommonSuccessResponse : BaseSuccessResponse
     {
         [JsonProperty("ConversationID")]
         public string ConversationID { get; set; }
 
         [JsonProperty("OriginatorConversationID")]
         public string OriginatorConversationID { get; set; }
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+
+    public class StkPushQuerySuccessResponse : StkPushSuccessResponse
+    {
+        [JsonProperty("ResultDesc")]
+        public string ResultDesc { get; set; }
+        [JsonProperty("ResultCode")]
+        public string ResultCode { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+
+    public class StkPushSuccessResponse : StkPushBaseSuccessResponse
+    {
+        [JsonProperty("CustomerMessage")]
+        public string CustomerMessage { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+
+    public class StkPushBaseSuccessResponse : BaseSuccessResponse
+    {
+        [JsonProperty("MerchantRequestID")]
+        public string MerchantRequestID { get; set; }
+        [JsonProperty("CheckoutRequestID")]
+        public string CheckoutRequestID { get; set; }
         public override string ToString()
         {
             return JsonConvert.SerializeObject(this);
